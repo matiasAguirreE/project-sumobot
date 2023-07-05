297bf02b3f8552283ab8035f8f4099f2fee04a74
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-07-05 12:34:47 -0400

    FEAT: Added declarations of pins

--- docs/code/sumobot.ino
+++ docs/code/sumobot.ino
@@ -1,1 +1,31 @@
+// Declaration of pins
 
+/** Pin of the left motor 
+ *
+ * It is the number of the pin used to control the left motor
+ */
+int motorLeftPin;
+
+/** Pin of the right motor 
+ *
+ * It is the number of the pin used to control the right motor
+ */
+int motorRightPin;
+
+/** Pin of the foward infrared 
+ *
+ * It is the number of the pin used to control the foward infrared
+ */
+int fowardInfraredPin;
+
+/** Pin of the rear infrared 
+ *
+ * It is the number of the pin used to control the rear infrared
+ */
+int rearInfraredPin;
+
+/** Pin of the ultrasonic sensor 
+ *
+ * It is the number of the pin used to control the ultrasonic sensor
+ */
+int ultrasonicPin;
