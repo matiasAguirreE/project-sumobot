2f17a889508eb325657676b77d8fd1dedff76c69
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-07-10 17:02:55 -0400

    FEAT: Added functions to checkout the border of the zone (not implemented yet)

--- docs/code/sumobot.ino
+++ docs/code/sumobot.ino
@@ -78,7 +78,84 @@ void setup() {
 void loop() {
   // If the button was pressed
   if (digitalRead(buttonPin) == LOW) {
-    delay(5000); // Delay to attack five seconds after the button was pressed
+   delay(5000); // Delay to attack five seconds after the button was pressed
+   // Look out the other sumobot to attack him
+   if (enemyIsNear()) {
     // Attack the other sumobot
+    attack();
+   } else {
+    findEnemy();
+   }
+   // Checkout if the sumobot is on the border 
+   if (onBorderFoward()) {
+    // Go away from the border
+    goAwayFoward();
+   }
+   if (onBorderRear()) {
+    // Go away from the border
+    goAwayRear();
+   }
   }
+}
+
+/** Function to attack the enemy
+  *
+  * Function with an algorithm to attack the enemy and avoid the attacks of the enemy 
+  */
+void attack() {
+ // Code to attack the enemy
+}
+
+/** Function to find the enemy
+  *
+  * Function that move the sumobot to find the enemy 
+  */
+void findEnemy() {
+ // Code to find the enemy
+}
+
+/** Function to tell if the enemy is on the front or not
+  *
+  * Function that use the ultrasonic sensor to detect if the enemy is in front of the sumobot or not
+  *
+  * @return True if the enemy is on the front and False in other cases
+  */
+bool enemyIsNear() {
+ // Code to detect the enemy
+}
+
+/** Function to tell if the border is on the foward or not
+  *
+  * Function that use the infrared sensor on the foward to detect if the border is on the foward of the sumobot or not
+  *
+  * @return True if the border is on the foward and False in other cases
+  */
+bool onBorderFoward() {
+ // Code to detect the border with the foward sensor
+}
+
+/** Function to tell if the border is on the rear or not
+  *
+  * Function that use the infrared sensor on the rear to detect if the border is on the rear of the sumobot or not
+  *
+  * @return True if the border is on the rear and False in other cases
+  */
+bool onBorderRear() {
+ // Code to detect the border with the rear sensor
+}
+
+/** Function to go away from the border of the foward
+  *
+  * Function that go back from the border of the foward moving on rear
+  */
+void goAwayFoward() {
+ // Code to go away from the border of the foward
+}
+
+/** Function to go away from the border of the rear
+  *
+  * Function that go back from the border of the rear moving on foward
+  */
+void goAwayRear() {
+ // Code to go away from the border of the rear
 }
